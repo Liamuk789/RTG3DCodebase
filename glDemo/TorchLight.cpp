3b5d6ba173d78c81e00b9973abdcf119e88fb723
Liamuk789 <liam.hancox@hotmail.co.uk>
2025-05-04 15:08:20 +0100

    torchlight edit

--- glDemo/TorchLight.cpp
+++ glDemo/TorchLight.cpp
@@ -2,7 +2,11 @@
 
 TorchLight::TorchLight()
 {
-
+	m_type = "TORCH";
+	m_pos = vec3(0.0, 0.0, 0.0);
+	m_col = vec3(0.0, 0.0, 1.0);
+	m_att = vec3(0.0, 0.0, 0.0);
+	m_noTorch = 0.0f;
 }
 
 TorchLight::~TorchLight()
--- glDemo/TorchLight.h
+++ glDemo/TorchLight.h
@@ -20,5 +20,5 @@ public:
 
 protected:
 
-
+	float m_noTorch;
 };
